f67fe3d302758115123da2185787777ee8ce7361
ShotaAk <macakasit@gmail.com>
2019-02-11 22:59:59 +0900

    Rename test file

--- consai2_receiver/scripts/tests/test_format_converter.py
+++ consai2_receiver/scripts/tests/test_format_converter.py
@@ -15,14 +15,6 @@ class TestFormatConverter(unittest.TestCase):
     def setUp(self):
         pass
 
-    def test_invert_check(self):
-
-        do_invert = False
-        converter = FormatConverter(do_invert)
-
-        self.assertEqual(do_invert, converter.do_invert())
-
-
 if __name__ == '__main__':
     import rosunit
     rosunit.unitrun('consai2_receiver', 'test_format_converter', TestFormatConverter)
