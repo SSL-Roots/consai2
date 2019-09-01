943fb661cb31c242b06969b7338093645c5ba7c0
shuwa watanabe <nabekuro0104@gmail.com>
2019-09-01 20:29:05 +0900

    field情報を受け取れない時は、デフォルトのフィールド情報を参照するように変更。

--- consai2_game/scripts/example/field.py
+++ consai2_game/scripts/example/field.py
@@ -12,15 +12,32 @@ class Field(object):
     _goal_pose = {
             'our': copy.deepcopy(_goal_dict), 
             'their': copy.deepcopy(_goal_dict)}
-    _penalty_dict = {
-            'upper_front' : Pose2D(), 'upper_back' : Pose2D(),
-            'lower_front' : Pose2D(), 'lower_back' : Pose2D(),}
+    # Division A仕様をデフォルトとして設定
+    _our_penalty_dict = {
+            'upper_front' : Pose2D(-4.795, 1.2, 0), 'upper_back' : Pose2D(-5.99, 1.2, 0),
+            'lower_front' : Pose2D(-4.795, -1.2, 0), 'lower_back' : Pose2D(-5.99, -1.2, 0),}
+    _their_penalty_dict = {
+            'upper_front' : Pose2D(4.795, 1.2, 0), 'upper_back' : Pose2D(5.99, 1.2, 0),
+            'lower_front' : Pose2D(4.795, -1.2, 0), 'lower_back' : Pose2D(5.99, -1.2, 0),}
     _penalty_pose = {
-            'our': copy.deepcopy(_penalty_dict), 
-            'their': copy.deepcopy(_penalty_dict)}
+            'our': copy.deepcopy(_our_penalty_dict), 
+            'their': copy.deepcopy(_their_penalty_dict)}
     _field = {
             'length' : 0,
             'width' : 0}
+    _geometry_field_lines = {}
+
+    # _penalty_poseとfield_lines紐付ける辞書
+    _field_lines_to_penalty_pose = {
+        'our_upper_front'  :["LeftPenaltyStretch",1],
+        'our_upper_back'   :["LeftFieldLeftPenaltyStretch",0],
+        'our_lower_front'  :["LeftPenaltyStretch",0],
+        'our_lower_back'   :["LeftFieldRightPenaltyStretch",0],
+        'their_upper_front':["RightPenaltyStretch",1],
+        'their_upper_back' :["RightFieldRightPenaltyStretch",0],
+        'their_lower_front':["RightPenaltyStretch",0],
+        'their_lower_back' :["RightFieldLeftPenaltyStretch",0],
+    }
 
     @classmethod
     def update(cls, geometry):
@@ -36,15 +53,9 @@ class Field(object):
         Field._goal_pose['their']['center'] = Pose2D(half_length, 0, 0)
         Field._goal_pose['their']['lower'] = Pose2D(half_length, -half_goal_width, 0)
 
-        # ペナルティエリア座標の更新
-        Field._penalty_pose['our']['upper_front'] = [Pose2D(i.p2_x, i.p2_y,0) for i in geometry.field_lines if i.name == "LeftPenaltyStretch"][0]
-        Field._penalty_pose['our']['upper_back']  = [Pose2D(i.p1_x, i.p1_y,0) for i in geometry.field_lines if i.name == "LeftFieldLeftPenaltyStretch"][0]
-        Field._penalty_pose['our']['lower_front'] = [Pose2D(i.p1_x, i.p1_y,0) for i in geometry.field_lines if i.name == "LeftPenaltyStretch"][0]
-        Field._penalty_pose['our']['lower_back']  = [Pose2D(i.p1_x, i.p1_y,0) for i in geometry.field_lines if i.name == "LeftFieldRightPenaltyStretch"][0]
-        Field._penalty_pose['their']['upper_front'] = [Pose2D(i.p2_x, i.p2_y,0) for i in geometry.field_lines if i.name == "RightPenaltyStretch"][0]
-        Field._penalty_pose['their']['upper_back'] = [Pose2D(i.p1_x, i.p1_y,0) for i in geometry.field_lines if i.name == "RightFieldRightPenaltyStretch"][0]
-        Field._penalty_pose['their']['lower_front'] = [Pose2D(i.p1_x, i.p1_y,0) for i in geometry.field_lines if i.name == "RightPenaltyStretch"][0]
-        Field._penalty_pose['their']['lower_back'] = [Pose2D(i.p1_x, i.p1_y,0) for i in geometry.field_lines if i.name == "RightFieldLeftPenaltyStretch"][0]
+        # geometry.field_linesに入っている座標データを更新
+        for field_lines_info in geometry.field_lines:
+            Field._geometry_field_lines[field_lines_info.name] = [Pose2D(field_lines_info.p1_x, field_lines_info.p1_y, 0), Pose2D(field_lines_info.p2_x, field_lines_info.p2_y, 0)] 
 
         # フィールドサイズ取得
         Field._field['length'] = geometry.field_length
@@ -56,6 +67,11 @@ class Field(object):
 
     @classmethod
     def penalty_pose(cls, team='our', position='upper_front'):
+        # geometry.field_linesにペナルティエリアに関する情報が入っていれば、更新する
+        if Field._field_lines_to_penalty_pose[team + '_' + position][0] in Field._geometry_field_lines:
+            geometry_field_lines_name = Field._field_lines_to_penalty_pose[team + '_' + position][0]
+            geometry_field_lines_p_num = Field._field_lines_to_penalty_pose[team + '_' + position][1]
+            Field._penalty_pose[team][position] = Field._geometry_field_lines[geometry_field_lines_name][geometry_field_lines_p_num]
         return Field._penalty_pose[team][position]
 
     @classmethod
