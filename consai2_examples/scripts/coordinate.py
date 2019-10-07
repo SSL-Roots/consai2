d3291a3c46a838da02019f4c38fd6a45899c5009
uchida <um2493@gmail.com>
2019-10-07 23:09:52 +0900

    add pk_atk

---
+++ consai2_examples/scripts/coordinate.py
@@ -0,0 +1,142 @@
+#!/usr/bin/env python2
+# coding: UTF-8
+
+import math
+import tool
+
+from geometry_msgs.msg import Pose2D
+
+RobotRadius = 0.09
+BallRadius = 0.0215
+
+OUR_GOAL_X = 0.9
+OUR_GOAL_Y = 0
+FRONT_GOAL_X = -0.9
+FRONT_GOAL_Y = 0
+
+class Coordinate(object):
+    # Coordinateクラスは、フィールド状況をもとに移動目標位置、目標角度を生成する
+    # Coordinateクラスには、移動目標の生成方法をsetしなければならない
+    # Coordinateクラスのposeが生成された移動目標である
+
+    def __init__(self):
+        self.pose = Pose2D() # pos_x, pos_y, thta
+
+        self._robot_pose = Pose2D()
+        self._ball_pose = Pose2D()
+        self.target_pose = Pose2D()
+
+        self._base = None # string data
+        self._target = None # string data
+
+        # approach to shoot
+        self._pose_max = Pose2D()
+        self._role_is_lower_side = False
+        self._role_pose_hystersis = 0.1
+        self._tuning_param_x = 0.3
+        self._tuning_param_y = 0.3
+        self._tuning_param_pivot_y = 0.1
+        self._tuning_angle = 80.0 * math.pi / 180.0  # 0 ~ 90 degree, do not edit 'math.pi / 180.0'
+
+        self._pose_max.x = BallRadius + self._tuning_param_x
+        self._pose_max.y = BallRadius + RobotRadius + self._tuning_param_y
+
+        self.approach_state = 0
+
+    def _update_robot_pose(self, robot_pose):
+        self._robot_pose = robot_pose
+
+    def _update_ball_pose(self, ball_pose):
+        self._ball_pose = ball_pose
+
+    def get_target_pose(self):
+        return self.target_pose
+
+    def _update_approach_to_shoot(self):
+        # Reference to this idea
+        # http://wiki.robocup.org/images/f/f9/Small_Size_League_-_RoboCup_2014_-_ETDP_RoboDragons.pdf
+
+        _target_pose = Pose2D(FRONT_GOAL_X, FRONT_GOAL_Y,0)
+        _role_pose = self._robot_pose
+
+        if _target_pose is None or _role_pose is None:
+            return False
+
+        # ボールからターゲットを見た座標系で計算する
+        angle_ball_to_target = tool.getAngle(self._ball_pose, _target_pose)
+        trans = tool.Trans(self._ball_pose, angle_ball_to_target)
+        tr_role_pose = trans.transform(_role_pose)
+
+        # tr_role_poseのloser_side判定にヒステリシスをもたせる
+        if self._role_is_lower_side == True and \
+                tr_role_pose.y > self._role_pose_hystersis:
+            self._role_is_lower_side = False
+
+        elif self._role_is_lower_side == False and \
+                tr_role_pose.y < - self._role_pose_hystersis:
+            self._role_is_lower_side = True
+
+        if self._role_is_lower_side:
+            tr_role_pose.y *= -1.0
+
+
+        tr_approach_pose = Pose2D()
+        if tr_role_pose.x > 0:
+            # 1.ボールの斜め後ろへ近づく
+            self.approach_state = 1
+
+            # copysign(x,y)でyの符号に合わせたxを取得できる
+            tr_approach_pose = Pose2D(
+                    -self._pose_max.x,
+                    math.copysign(self._pose_max.y, tr_role_pose.y), 
+                    0)
+
+        else:
+            # ボール裏へ回るためのピボットを生成
+            pivot_pose = Pose2D(0, self._tuning_param_pivot_y, 0)
+            angle_pivot_to_role = tool.getAngle(pivot_pose,tr_role_pose)
+
+            limit_angle = self._tuning_angle + math.pi * 0.5
+
+            if tr_role_pose.y > self._tuning_param_pivot_y and \
+                    angle_pivot_to_role < limit_angle:
+                # 2.ボール後ろへ回りこむ
+                self.approach_state = 2
+            
+                diff_angle = tool.normalize(limit_angle - angle_pivot_to_role)
+                decrease_coef = diff_angle / self._tuning_angle
+            
+                tr_approach_pose = Pose2D(
+                        -self._pose_max.x,
+                        self._pose_max.y * decrease_coef, 
+                        0)
+            
+            else:
+                # 3.ボールに向かう
+                self.approach_state = 3
+
+                diff_angle = tool.normalize(angle_pivot_to_role - limit_angle)
+                approach_coef = diff_angle / (math.pi * 0.5 - self._tuning_angle)
+            
+                if approach_coef > 1.0:
+                    approach_coef = 1.0
+
+                pos_x = approach_coef * (2.0 * BallRadius 
+                        - self._tuning_param_x) + self._tuning_param_x
+                
+                # ずれて肩タックルするときはこれをいじる
+                tr_approach_pose = Pose2D(-pos_x, 0, 0)
+                # print pos_x
+
+        # 上下反転していたapproach_poseを元に戻す
+        if self._role_is_lower_side:
+            tr_approach_pose.y *= -1.0
+
+        self.pose = trans.invertedTransform(tr_approach_pose)
+        self.pose.theta = angle_ball_to_target
+
+        self.target_pose = self.pose
+        
+        return True
+
+
---
+++ consai2_examples/scripts/pk_atk.py
@@ -0,0 +1,249 @@
+#!/usr/bin/env python2
+# coding: UTF-8
+
+import rospy
+import copy
+
+from consai2_msgs.msg import ControlTarget, RobotCommands, RobotCommand
+from geometry_msgs.msg import Pose2D
+from consai2_msgs.msg import VisionDetections, VisionGeometry, BallInfo, RobotInfo
+
+import joystick_example
+import coordinate
+
+import math
+
+ball_pose = Pose2D()
+robot_pose = Pose2D()
+target_pose = Pose2D()
+
+# 制御目標値を生成
+control_target = ControlTarget()
+command = RobotCommand()
+
+# ボールを取得したかどうかのFlag
+ball_get_state = 1
+
+def path_example(target_id, coordinate, joy_wrapper, button, ang_vel):
+    # 制御目標値を生成
+    global control_target, target_pose, command, robot_pose, ball_pose, ball_get_state
+    control_target = ControlTarget()
+
+    robot_commands = RobotCommands()
+    robot_commands.header.stamp = rospy.Time.now()
+
+    _robot_pose = robot_pose
+    _ball_pose = ball_pose
+
+    # ロボットID
+    control_target.robot_id = target_id
+    # Trueで走行開始
+    control_target.control_enable = True
+
+    # 経路生成
+    coordinate._update_approach_to_shoot()
+    # ロボットとボールの間の距離
+    dist = distance_2_poses(_robot_pose, _ball_pose)
+    # ロボットとボール間の相対角度(degで取得)
+    angle_rb = angle_2_diff(_robot_pose.theta, angle_2_poses(_robot_pose, _ball_pose), unit='deg')
+
+    dist_th = 0.13
+    ang_th = 30
+
+    kick_flag = False
+    # ボールがしきい値内かどうか判定
+    # print(dist, angle_rb)
+    if dist < dist_th and abs(angle_rb) < ang_th:
+        # print(ball_get_state, dist, angle_rb)
+        if ball_get_state == 1:
+            ang = angle_2_poses(_robot_pose, _ball_pose)
+            dx = 0.05 * math.cos(ang*math.pi/180)
+            dy = 0.05 * math.sin(ang*math.pi/180)
+            target_pose.x = ball_pose.x + dx
+            target_pose.y = ball_pose.y + dy
+            control_target.path.append(target_pose)
+            ball_get_state = 2
+            print('1')
+           
+        elif ball_get_state == 2: 
+
+            control_target.dribble_power = 0
+            control_target.path.append(target_pose)
+            if distance_2_poses(_robot_pose, target_pose) < 0.03:
+                ball_get_state = 3
+            print('2')
+        else:
+            # 角度調整
+            command.robot_id = target_id
+            command.vel_surge = 0 
+            command.vel_sway = 0
+            command.vel_angular = ang_vel * math.pi * 0.25
+
+            command.dribble_power = 0.5
+            # 指定角度以内 + ボタン入力がある場合蹴る
+            if abs(angle_rb) < 20 and button:
+                command.kick_power = 0.3
+                ball_get_state = 1
+                kick_flag = True
+            else:
+                command.kick_power = 0
+            print('3')
+
+            robot_commands.commands.append(copy.deepcopy(command))
+            joy_wrapper._pub_commands.publish(robot_commands)
+
+    # しきい値内では無い場合経路生成してボールまで行く
+    else:
+        ball_get_state = 1
+
+        control_target.dribble_power = 0
+        control_target.kick_power = 0
+        target_pose = coordinate.get_target_pose()
+        control_target.path.append(target_pose)
+        print('4')
+
+    return control_target, kick_flag
+
+# コントローラを離しているときは停止
+def stop(target_id):
+    
+    global control_target
+    
+    control_target.robot_id = target_id
+    control_target.control_enable = False
+    
+    return control_target
+
+
+def BallPose(data):
+    global ball_pose
+    ball_pose = data.pose
+
+
+def RobotPose(data):
+    global robot_pose
+    robot_pose = data.pose
+
+
+# 2つの角度の差分を取る
+def angle_2_diff(angle_1, angle_2, unit='rad'):
+    angle_diff = (angle_1 - angle_2) 
+    angle_diff = angle_normalize(angle_diff)
+
+    # 単位をdegに直す
+    if unit == 'deg':
+        angle_diff *= 180/math.pi
+
+    return angle_diff
+
+
+# 角度を-pi ~ piに変換
+def angle_normalize(angle):
+    while angle > math.pi:
+        angle -= 2*math.pi
+
+    while angle < -math.pi:
+        angle += 2*math.pi
+
+    return angle
+
+
+# 角度を計算する
+# ２点間の直線と水平線との角度
+def angle_2_poses(pose1, pose2):
+    diff_pose = Pose2D()
+
+    diff_pose.x = pose2.x - pose1.x
+    diff_pose.y = pose2.y - pose1.y
+
+    return math.atan2(diff_pose.y, diff_pose.x)
+
+
+# 2点間の距離を計算する
+def distance_2_poses(pose1, pose2):
+    # 2点間の距離を取る
+    # pose.theta は使用しない
+
+    diff_pose = Pose2D()
+
+    diff_pose.x = pose1.x - pose2.x
+    diff_pose.y = pose1.y - pose2.y
+
+    return math.hypot(diff_pose.x, diff_pose.y)
+
+
+def main():
+    rospy.init_node('control_example')
+    MAX_ID = rospy.get_param('consai2_description/max_id', 15)
+    COLOR = "blue" # 'blue' or 'yellow'
+    TARGET_ID = 0 # 0 ~ MAX_ID
+
+    # 末尾に16進数の文字列をつける
+    topic_id = hex(TARGET_ID)[2:]
+    topic_name = 'consai2_game/control_target_' + COLOR +'_' + topic_id
+
+    topic_name_robot_info = 'vision_wrapper/robot_info_' + COLOR + '_' + str(TARGET_ID)
+
+    _coordinate = coordinate.Coordinate()
+
+    # print sub
+    pub = rospy.Publisher(topic_name, ControlTarget, queue_size=1)
+    pub_joy = rospy.Publisher('consai2_examples/joy_target', ControlTarget, queue_size=1)
+
+    # ballの位置を取得する
+    sub_ball = rospy.Subscriber('vision_wrapper/ball_info', BallInfo, BallPose)
+    # Robotの位置を取得する
+    sub_robot = rospy.Subscriber(topic_name_robot_info, RobotInfo, RobotPose)
+
+    # joy_
+    joy_wrapper = joystick_example.JoyWrapper()
+    print 'control_exmaple start'
+
+    rospy.sleep(3.0)
+
+    # 制御目標値を生成
+    r = rospy.Rate(60)
+
+    kick_flag = False
+    button_flag = 0
+    while 1:
+        if joy_wrapper.get_button_status() != None:
+            _joy_msg = joy_wrapper.get_button_status()
+            button_lb = _joy_msg.buttons[4]
+            button_x  = _joy_msg.buttons[0]
+            ang_vel   = _joy_msg.axes[3] 
+        else:
+            button_lb = 0
+            button_x  = 0
+            ang_vel   = 0
+
+        _coordinate._update_robot_pose(robot_pose)
+        _coordinate._update_ball_pose(ball_pose)
+
+        if button_lb and kick_flag == False:
+            # パスの生成
+            control_target, kick_flag = path_example(
+                                                TARGET_ID,
+                                                _coordinate,
+                                                joy_wrapper,
+                                                button_x,
+                                                ang_vel)
+
+            pub.publish(control_target)
+            
+        elif button_flag == 1 and button_lb == 0:
+            kick_flag = False
+        else:
+            control_target = stop(TARGET_ID)
+            pub.publish(control_target)
+            pub_joy.publish(control_target)
+
+        button_flag = button_lb
+
+        r.sleep()
+
+    print 'control_exmaple finish'
+
+
+if __name__ == '__main__':
+    main()
