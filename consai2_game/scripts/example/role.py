1d636af83963e3b1396397be8c3bc6e16c488452
shuwa watanabe <nabekuro0104@gmail.com>
2020-02-25 00:29:36 +0900

    とりあえず動くように

--- consai2_game/scripts/example/role.py
+++ consai2_game/scripts/example/role.py
@@ -164,12 +164,16 @@ def role_decision(my_role, ball_info, control_target, my_pose, defense_num, robo
     if ROLE_ID['ROLE_CENTER_BACK_1'] <= my_role <= ROLE_ID['ROLE_CENTER_BACK_2']:
         return center_back.center_back(my_pose, ball_info, control_target, my_role, defense_num)
     elif ROLE_ID['ROLE_SUB_ATTACKER']:
-        pass
+        control_target.path = []
+        control_target.path.append(my_pose)
+        return control_target
     # ゾーンディフェンス
     elif ROLE_ID['ROLE_ZONE_1'] <= my_role <= ROLE_ID['ROLE_ZONE_4']:
         return zone.defense_zone(my_pose, ball_info, control_target, my_role, defense_num, robot_info['their'], zone_enable)
     elif ROLE_ID['ROLE_MAN_MARK_1'] <= my_role <= ROLE_ID['ROLE_MAN_MARK_2']:
-        pass
+        control_target.path = []
+        control_target.path.append(my_pose)
+        return control_target
     # 例外だった場合はその場にいる
     else:
         control_target.path = []
