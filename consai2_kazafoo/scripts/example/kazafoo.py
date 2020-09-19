d934b45631ea715ee2cc763cb08b92053d179e29
tilt-silvie <tilt.silvie@gmail.com>
2020-09-19 23:03:55 +0900

    kazafooからの受信処理を実装

--- consai2_kazafoo/scripts/example/kazafoo.py
+++ consai2_kazafoo/scripts/example/kazafoo.py
@@ -9,10 +9,43 @@ import rospy
 from sensor_msgs.msg import Joy
 
 import sys, select, termios, tty
+import serial
+
+import time
 
 moveBindings = {
         'b':(0,0,-1,0),
     }
+
+
+class KazafooCom(threading.Thread):
+    def __init__(self):
+        super(KazafooCom, self).__init__()
+        self.daemon = True
+
+        self._DEVICE  = rospy.get_param('consai2_description/kazafoo_device', '/dev/ttyACM0')
+        self._BAUDRATE   = rospy.get_param('consai2_description/kazafoo_device', 9600)
+
+        self._serial = serial.Serial(self._DEVICE, self._BAUDRATE)
+
+        self.left_value = 0.0
+        self.right_value = 0.0
+
+    def run(self):
+        while(1):
+            data = self._serial.readline()
+            left_value_raw = data.split(',')[0]
+            right_value_raw = data.split(',')[1]
+
+            try:
+                self.left_value = float(left_value_raw)
+            except:
+                self.left_value = 0.0
+            try:
+                self.right_value = float(right_value_raw)
+            except:
+                self.right_value = 0.0
+
 
 class PublishThread(threading.Thread):
     def __init__(self, rate):
@@ -75,39 +108,48 @@ def getKey(key_timeout):
     return key
 
 if __name__=="__main__":
-    settings = termios.tcgetattr(sys.stdin)
+    # settings = termios.tcgetattr(sys.stdin)
 
     rospy.init_node('kazafoo_node')
 
-    speed = rospy.get_param("~speed", 0.5)
-    turn = rospy.get_param("~turn", 1.0)
-    repeat = rospy.get_param("~repeat_rate", 0.0)
-    key_timeout = rospy.get_param("~key_timeout", 0.0)
-    if key_timeout == 0.0:
-        key_timeout = 0.1
+    # speed = rospy.get_param("~speed", 0.5)
+    # turn = rospy.get_param("~turn", 1.0)
+    # repeat = rospy.get_param("~repeat_rate", 0.0)
+    # key_timeout = rospy.get_param("~key_timeout", 0.0)
+    # if key_timeout == 0.0:
+    #     key_timeout = 0.1
 
-    pub_thread = PublishThread(repeat)
+    # pub_thread = PublishThread(repeat)
+
+    kazafoo_com_thread = KazafooCom()
+    kazafoo_com_thread.start()
+
+    r = rospy.Rate(10)
+    while not rospy.is_shutdown():
+        print("looping")
+        print(kazafoo_com_thread.left_value)
+        print(kazafoo_com_thread.right_value)
+        r.sleep()
 
     kick_flag = False
+    # try:
+    #     pub_thread.update(kick_flag)
 
-    try:
-        pub_thread.update(kick_flag)
-
-        while(1):
-            key = getKey(key_timeout)
-            if key == 'b':
-                kick_flag = True
-            else:
-                kick_flag = False
-                if (key == '\x03'):
-                    break
+    #     while(1):
+    #         key = getKey(key_timeout)
+    #         if key == 'b':
+    #             kick_flag = True
+    #         else:
+    #             kick_flag = False
+    #             if (key == '\x03'):
+    #                 break
  
-            pub_thread.update(kick_flag)
+    #         pub_thread.update(kick_flag)
 
-    except Exception as e:
-        print(e)
+    # except Exception as e:
+    #     print(e)
 
-    finally:
-        pub_thread.stop()
+    # finally:
+    #     pub_thread.stop()
 
-        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+    #     termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
